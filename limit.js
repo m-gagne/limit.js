c6f2fc94b0553c96c2fd8044f28bbf9cee342407
Nathan Bishop <nbish11@hotmail.com>
2015-01-19 11:27:53 +1000

    Added a way to provide the context for this in debounce()

--- limit.js
+++ limit.js
@@ -1,18 +1,19 @@
 /**
  * debounce
  * @param {integer} milliseconds This param indicates the number of milliseconds
- *     to wait after the last call before calling the original function .
+ *     to wait after the last call before calling the original function.
+ * @param {object} What "this" refers to in the returned function.
  * @return {function} This returns a function that when called will wait the
  *     indicated number of milliseconds after the last call before
  *     calling the original function.
  */
-Function.prototype.debounce = function (milliseconds) {
+Function.prototype.debounce = function (milliseconds, context) {
     var baseFunction = this,
         timer = null,
         wait = milliseconds;
 
     return function () {
-        var self = this,
+        var self = context || this,
             args = arguments;
 
         function complete() {
